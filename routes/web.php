cd9ac90e0891bb018d33deadb5772610434bc8e4
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-16 17:24:08 +0500

    shopping cart added

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 //});
 Route::get('/', HomeComponent::class);
 Route::get('/shop', ShopComponent::class);
-Route::get('/cart', CartComponent::class);
+Route::get('/cart', CartComponent::class)->name('product.cart');
 Route::get('/checkout', CheckoutComponent::class);
 
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
