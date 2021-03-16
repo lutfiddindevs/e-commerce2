7904a09cca4eef040cbc299d8a822ddf01b8f465
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-16 17:25:13 +0500

    added a store function

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Product;
+use Cart;
 
 class DetailsComponent extends Component
 {   public $slug;
@@ -11,6 +12,12 @@ class DetailsComponent extends Component
     public function mount($slug) {
     		$this->slug = $slug;
     } 
+
+    public function store($product_id, $product_name, $product_price) {
+        Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
+        session()->flash('success_message', 'Item added in Cart');
+        return redirect()->route('product.cart');
+    }
     
     public function render()
     {
