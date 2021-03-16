92789c2960d4b4b83a9b16a13ecc50deab42f315
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-16 17:28:54 +0500

    added a number of items in a cart to header

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -154,7 +154,7 @@
                             <a href="#" class="link-direction">
                                 <i class="fa fa-heart" aria-hidden="true"></i>
                                 <div class="left-info">
-                                    <span class="index">0 item</span>
+                                    <span class="index">0 items</span>
                                     <span class="title">Wishlist</span>
                                 </div>
                             </a>
@@ -163,7 +163,9 @@
                             <a href="#" class="link-direction">
                                 <i class="fa fa-shopping-basket" aria-hidden="true"></i>
                                 <div class="left-info">
-                                    <span class="index">4 items</span>
+                                     @if(Cart::count() > 0)
+                                    <span class="index">{{ Cart::count() }} items</span>
+                                    @endif
                                     <span class="title">CART</span>
                                 </div>
                             </a>
