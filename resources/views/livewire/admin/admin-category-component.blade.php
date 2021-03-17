f3a19f58585d6627109b55ad2a131c25a3dc03b6
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-18 00:13:21 +0500

    admin category page

---
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -0,0 +1,44 @@
+<div>
+	<style type="text/css">
+		nav svg{
+			height: 20px;
+		}
+		nav .hidden {
+			display: block !important;
+		}
+	</style>
+    <div class="container" style="padding: 30px 0;">
+    	<div class="row">
+    		<div class="col-md-12">
+    			<div class="panel panel-default">
+    				<div class="panel-heading">
+    					All Categories 
+    				</div>
+    				<div class="panel-body">
+    					<table class="table table-striped">
+    						<thead>
+    							<tr>
+    								<td>Id</td>
+    								<td>Category Name</td>
+    								<td>Slug</td>
+    								<td>Action</td>
+    							</tr>
+    						</thead>
+    						<tbody>
+    							@foreach($categories as $category)
+    							<tr>
+    								<td>{{ $category->id }}</td>
+    								<td>{{ $category->name }}</td>
+    								<td>{{ $category->slug }}</td>
+    								<td></td>
+    							</tr>
+    							@endforeach
+    						</tbody>
+    					</table>
+    					{{ $categories->links() }}
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+</div>
