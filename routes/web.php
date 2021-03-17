fb30e7e22ad335ff71a4b1ae69039ec78dbe2f48
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-17 17:46:44 +0500

    product-category

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 
@@ -43,3 +44,4 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function() {
 }); 
 
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
+Route::get('/product-category/{category_slug}', CategoryComponent::class)->name('product.category');
