b2a7ff4f18d8490bfa07608e71d46516385b721d
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-18 00:12:23 +0500

    AdminCategoryComponent class

---
+++ app/Http/Livewire/Admin/AdminCategoryComponent.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+use App\Models\Category;
+use Livewire\WithPagination;
+
+class AdminCategoryComponent extends Component
+{
+	use WithPagination;
+    public function render()
+    {   
+    	$categories = Category::paginate(5);
+        return view('livewire.admin.admin-category-component', ['categories' => $categories])->layout('layouts.base');
+    }
+}
