ef5c14cb2caf486e944b3611383ee3be8e1cf7a1
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-18 17:52:37 +0500

    add new category

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -44,6 +45,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function() {
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function() {
      Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
      Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
+     Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
 }); 
 
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
