78b600f8603cd1a21891e0dfa71cf8ddf421ba22
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-18 22:33:43 +0500

    all products link

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -79,6 +79,9 @@
                                             <li class="menu-item">
                                                 <a title="Categories" href="{{ route('admin.categories') }}">Categories</a>
                                             </li>
+                                            <li class="menu-item">
+                                                <a title="Products" href="{{ route('admin.products') }}">All Products</a>
+                                            </li>
                                                  <li class="menu-item" >
                                                 <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                                             </li>
