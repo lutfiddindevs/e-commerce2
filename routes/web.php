8a217892276dd1dc18e59820553e9bcbb45c197f
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-18 22:32:09 +0500

    admin product

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
+use App\Http\Livewire\Admin\AdminProductComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,6 +49,7 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function() {
      Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
      Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
      Route::get('/admin/category/edit/{category_slug}', AdminEditCategoryComponent::class)->name('admin.editcategory');
+     Route::get('/admin/products', AdminProductComponent::class)->name('admin.products');
 }); 
 
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
