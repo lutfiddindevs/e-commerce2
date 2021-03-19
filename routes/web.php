0c24b4456685336418eb50e773d34c72c0aa9cb8
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-19 16:04:53 +0500

    admin add product

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
-
+use App\Http\Livewire\Admin\AdminAddProductComponent;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -50,6 +50,7 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function() {
      Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
      Route::get('/admin/category/edit/{category_slug}', AdminEditCategoryComponent::class)->name('admin.editcategory');
      Route::get('/admin/products', AdminProductComponent::class)->name('admin.products');
+     Route::get('/admin/product/add', AdminAddProductComponent::class)->name('admin.addproduct');
 }); 
 
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
