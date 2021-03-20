dbb31ea32edc4c0ab835d8b0a84f4e1c0efde69c
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-21 00:23:37 +0500

    manage home slider

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -82,6 +82,9 @@
                                             <li class="menu-item">
                                                 <a title="Products" href="{{ route('admin.products') }}">All Products</a>
                                             </li>
+                                            <li class="menu-item">
+                                                <a title="Manage Home Slider" href="{{ route('admin.homeslider') }}">Manage Home Slider</a>
+                                            </li>
                                                  <li class="menu-item" >
                                                 <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                                             </li>
