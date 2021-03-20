c848566e96ea4ffe7f1d18bfbc9b7526fcb5af8c
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-21 00:24:16 +0500

    admin home sliders

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\Admin\AdminAddProductComponent;
 use App\Http\Livewire\Admin\AdminEditProductComponent;
+use App\Http\Livewire\Admin\AdminHomeSliderComponent;
+use App\Http\Livewire\Admin\AdminAddHomeSliderComponent;
+use App\Http\Livewire\Admin\AdminEditHomeSliderComponent;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -53,6 +56,9 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function() {
      Route::get('/admin/products', AdminProductComponent::class)->name('admin.products');
      Route::get('/admin/product/add', AdminAddProductComponent::class)->name('admin.addproduct');
      Route::get('/admin/product/edit/{product_slug}', AdminEditProductComponent::class)->name('admin.editproduct');
+     Route::get('/admin/slider', AdminHomeSliderComponent::class)->name('admin.homeslider');
+     Route::get('/admin/slider/add', AdminAddHomeSliderComponent::class)->name('admin.addhomeslider');
+     Route::get('/admin/slider/edit/{slide_id}', AdminEditHomeSliderComponent::class)->name('admin.edithomeslider');
 }); 
 
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
