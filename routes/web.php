e93c78b4068477f30ab641ad667ccb806df5f25a
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-20 12:10:04 +0500

    admin edit product

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\Admin\AdminAddProductComponent;
+use App\Http\Livewire\Admin\AdminEditProductComponent;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -51,6 +52,7 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function() {
      Route::get('/admin/category/edit/{category_slug}', AdminEditCategoryComponent::class)->name('admin.editcategory');
      Route::get('/admin/products', AdminProductComponent::class)->name('admin.products');
      Route::get('/admin/product/add', AdminAddProductComponent::class)->name('admin.addproduct');
+     Route::get('/admin/product/edit/{product_slug}', AdminEditProductComponent::class)->name('admin.editproduct');
 }); 
 
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
