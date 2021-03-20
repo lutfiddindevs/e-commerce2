3068106e07afca8b57f1dc15b825b8dcf597d4e2
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-20 14:40:55 +0500

    admin delete product

--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -21,6 +21,11 @@
                     </div>
     			</div>
     			<div class="panel-body">
+                    @if(Session::has('message'))
+                      <div class="alert alert-success" role="alert">
+                          {{ Session::get('message') }}
+                      </div>
+                    @endif
     				<table class="table table-striped">
     					<thead>
     						<tr>
@@ -45,7 +50,8 @@
     							<td>{{ $product->category->name }}</td>
     							<td>{{ $product->created_at }}</td>
     							<td>
-                                   <a href="{{ route('admin.editproduct', ['product_slug' => $product->slug]) }}"><i class="fa fa-edit fa-2x text-info"></i></a>                     
+                                   <a href="{{ route('admin.editproduct', ['product_slug' => $product->slug]) }}"><i class="fa fa-edit fa-2x text-info"></i></a>
+                                   <a href="#" style="margin-left: 10px;" wire:click.prevent="deleteProduct({{ $product->id }})"><i class="fa fa-times fa-2x text-danger"></i></a>                 
                                 </td>
     						</tr>
     						@endforeach
