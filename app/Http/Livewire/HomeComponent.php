ece2ac3b9c218f8048ef45b9e35ec500254d8f06
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-21 16:51:19 +0500

    fetching products from db in desc order

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -4,12 +4,14 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\HomeSlider;
+use App\Models\Product;
 
 class HomeComponent extends Component
 {
     public function render()
     {
     	$sliders = HomeSlider::where('status', 1)->get();
-        return view('livewire.home-component', ['sliders' => $sliders])->layout('layouts.base');
+    	$lproducts = Product::orderBy('created_at', 'DESC')->get()->take(8);
+        return view('livewire.home-component', ['sliders' => $sliders, 'lproducts' => $lproducts])->layout('layouts.base');
     }
 }
