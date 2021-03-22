d0d06fdff71ac346f77f3e56e4c43c45846caaa4
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-23 00:16:02 +0500

    admin adding confirmation on record deletion

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -44,7 +44,7 @@
     								<td>{{ $category->slug }}</td>
     								<td>
                                       <a href="{{ route('admin.editcategory', ['category_slug' => $category->slug]) }}"><i class="fa fa-edit fa-2x"></i></a>
-                                      <a href="#" wire:click.prevent="deleteCategory({{ $category->id }})" style="margin-left: 10px;"><i class="fa fa-times fa-2x text-danger"></i></a>
+                                      <a href="#" onclick="confirm('Are you sure to delete this category?') || event.stopImmediatePropagation()"  wire:click.prevent="deleteCategory({{ $category->id }})" style="margin-left: 10px;"><i class="fa fa-times fa-2x text-danger"></i></a>
                                     </td>
                                       
     							</tr>
