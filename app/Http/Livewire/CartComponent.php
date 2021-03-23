46709c4057591d56976788926fb128ec19774731
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-23 18:05:30 +0500

    auto refresh wishlist and cart count

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -11,21 +11,25 @@ class CartComponent extends Component
     	$product = Cart::instance('cart')->get($rowId);
     	$qty = $product->qty + 1;
     	Cart::instance('cart')->update($rowId, $qty);
+        $this->emitTo('cart-count-component','refreshComponent');
     }
 
     public function decreaseQuantity($rowId) {
     	$product = Cart::instance('cart')->get($rowId);
     	$qty = $product->qty - 1;
     	Cart::instance('cart')->update($rowId, $qty);
+        $this->emitTo('cart-count-component','refreshComponent');
     }
 
     public function destroy($rowId) {
         Cart::instance('cart')->remove($rowId);
+        $this->emitTo('cart-count-component','refreshComponent');
         session()->flash('success_message', 'Item has been successfully removed');
     }
 
     public function destroyAll() {
          Cart::instance('cart')->destroy();
+         $this->emitTo('cart-count-component','refreshComponent');
     }
 
     public function render()
