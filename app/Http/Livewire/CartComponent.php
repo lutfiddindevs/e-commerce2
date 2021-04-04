a54eb91e3f192b5a9f9a4cfb69452a3bd20b8be1
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-04 19:26:48 +0500

    add saveForLater option on Cart Page

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -32,6 +32,27 @@ class CartComponent extends Component
          $this->emitTo('cart-count-component','refreshComponent');
     }
 
+    public function switchToSaveForLater($rowId) {
+        $item = Cart::instance('cart')->get($rowId);
+        Cart::instance('cart')->remove($rowId);
+        Cart::instance('saveForLater')->add($item->id, $item->name, 1, $item->price)->associate('App\Models\Product');
+        $this->emitTo('cart-count-component','refreshComponent');
+        session()->flash('success_message','Item has been saved for later');
+    }
+
+    public function moveToCart($rowId) {
+        $item = Cart::instance('saveForLater')->get($rowId);
+        Cart::instance('saveForLater')->remove($rowId);
+        Cart::instance('cart')->add($item->id, $item->name, 1, $item->price)->associate('App\Models\Product');
+        $this->emitTo('cart-count-component','refreshComponent');
+        session()->flash('s_success_message','Item has been moved to cart');
+    }
+
+    public function deleteFromSaveForLater($rowId) {
+        Cart::instance('saveForLater')->remove($rowId);
+        session()->flash('s_success_message', 'Item has been removed from save for later');
+    }
+
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');
